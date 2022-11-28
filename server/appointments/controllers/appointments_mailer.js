0addbe613bcec9e56e013ac74d10a41de2118837
askan <askan@student.chalmers.se>
2022-11-28 16:36:32 +0100

    addition of images in email Helps #20

--- server/appointments/controllers/appointments_mailer.js
+++ server/appointments/controllers/appointments_mailer.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
 });
 
 //Dummy test data
-/*
+
 const recipient = "burakaskan2001@gmail.com"
 const timeslot = {
     Dentist: "john",
@@ -21,30 +21,41 @@ const clinic = {
     addressEmail: "somegmail",
     phoneNumber: "072312"
 }
-*/
+
+
+const option = {
+    from: "DentistryAutomated@hotmail.com",
+    to: recipient,
+    subject: "Your Dentist Appointment",
+    text: "You have booked a time with "
+        + timeslot.Dentist +
+        ". The time slot is from "
+        + timeslot.StartingTime
+        + " to " + timeslot.EndingTime
+        + ". The clinic is located in "
+        + clinic.addressLocation + ". "
+        + "For contacting the clinic either email: " + clinic.addressEmail
+        + " or call: " + clinic.phoneNumber
+        + ". Hope you have enjoyed our service and use our website again for dental bookings!",
+    html: '<b>You have booked a time with timeslot.Dentist </b><img src = "cid:toothLogo"/>',
+    attachments: [
+        {
+            filename: 'toothLogo.png',
+            path:'./toothLogo.png',
+            cid: 'toothLogo'
+        }
+    ]
+}
 
 transporter.sendMail(option, async function(err, info) {
     if (err) {
         console.log('Email could not be sent.')
+        console.log(err)
         return;
     }
     console.log("Sent: " + info.response)
 })
 
 function sendAppointmentMail (recipient, timeslot, clinic) {
-    const option = {
-        from: "DentistryAutomated@hotmail.com",
-        to: recipient,
-        subject: "Your Dentist Appointment",
-        text: "You have booked a time with "
-            + timeslot.Dentist +
-            ". The time slot is from "
-            + timeslot.StartingTime
-            + " to " + timeslot.EndingTime
-            + ". The clinic is located in "
-            + clinic.addressLocation + ". "
-            + "For contacting the clinic either email: " + clinic.addressEmail
-            + " or call: " + clinic.phoneNumber
-            + ". Hope you have enjoyed our service and use our website again for dental bookings!"
-    }
+
 }
