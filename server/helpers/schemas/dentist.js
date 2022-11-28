b32204a399407b3254f8166c7885396d44f546e9
aieh <aieh@student.chalmers.net>
2022-11-28 21:40:44 +0400

    moved schemas and tried testing

--- server/helpers/schemas/dentist.js
+++ server/helpers/schemas/dentist.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const dentistSchema = new Schema({
-    _id: Schema.Types.ObjectId,
     lunchHour: String,
     fikaHour: String
 })
