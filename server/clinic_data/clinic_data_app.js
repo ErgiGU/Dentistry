dc163f62833c1cfbee933f320f7e083cf89c5236
askan <askan@student.chalmers.se>
2022-11-29 14:19:42 +0100

    Preliminary backend for Map Helps #29

--- server/clinic_data/clinic_data_app.js
+++ server/clinic_data/clinic_data_app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const mqttHandler = require('../helpers/mqtt_handler');
 const config = require('../helpers/config');
+const clinicData = require('../helpers/schemas/clinic')
 
 // Variables
 const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://' + config.clinicUser.name + ':' + config.clinicUser.password + '@cluster0.lj881zv.mongodb.net/?retryWrites=true&w=majority';
@@ -36,6 +37,28 @@ mqttClient.mqttClient.on('message', function (topic, message) {
             mqttClient.sendMessage('testAppointment', 'Testing callback')
             break;
     }
+    if (topic === 'mapDataRequest') {
+        mapDataRequest()
+    }
 });
 
+function mapDataRequest() {
+    let clinicNames = []
+    let clinicCoordinates = []
+    let clinicDescrip = []
+    clinicData.find(function (err, clinic) {
+        try {
+            if (err) {
+                return next(err);
+            }
+            clinicNames.push(clinic.name)
+            clinicCoordinates.push(clinic.coordinates)
+            clinicDescrip.push(clinic.descripText)
+        }catch(err) {
+            console.log(err)
+            mqttClient.sendMessage('mapDataResponse', err)
+        }
+    })
+    mqttClient.sendMessage('mapDataResponse', (clinicData.name, clinicData.coordinates, clinicData.descripText))
+}
 module.exports = mqttClient;
