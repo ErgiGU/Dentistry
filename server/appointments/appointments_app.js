94ff54604d61c6a8aaaab97722f33bae3b06db63
askan <askan@student.chalmers.se>
2022-11-29 13:43:11 +0100

    Linking email method with back-end MQTT Helps #20

--- server/appointments/appointments_app.js
+++ server/appointments/appointments_app.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const mqttHandler = require('../helpers/mqtt_handler');
 const config = require('../helpers/config');
+const {appointments_mailer} = require("./controllers/appointments_mailer");
 
 // Variables
 const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://' + config.appointmentUser.name + ':' + config.appointmentUser.password + '@cluster0.lj881zv.mongodb.net/?retryWrites=true&w=majority';
 //const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/UserDB';
 
 // MQTT Client
+const mailer = new appointments_mailer
 const mqttClient = new mqttHandler(config.appointmentUser.handler)
 mqttClient.connect()
 
@@ -40,8 +42,8 @@ mqttClient.mqttClient.on('message', function (topic, message) {
 
 function bookAppointment(input) {
     // mongodb manipulation
-
-
+    mailer.sendAppointmentMail(input.recipient, input.timeslot, input.clinic)
+    mqttClient.sendMessage('bookAppointment', 'ADD MESSAGE HERE') //TODO: Add a message here.
     //mqtt message response
 }
 
