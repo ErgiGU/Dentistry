1fb1be4e4d9a14225e9a7a638c93321c45d6f6ab
askan <askan@student.chalmers.se>
2022-11-29 13:43:11 +0100

    Linking email method with back-end MQTT Helps #20

--- server/appointments/appointments_app.js
+++ server/appointments/appointments_app.js
@@ -1,9 +1,30 @@
+const mongoose = require('mongoose');
 const mqttHandler = require('../helpers/mqtt_handler');
 const config = require('../helpers/config');
+const timeslotSchema = require('../helpers/schemas/timeslot')
+const {appointments_mailer} = require("./controllers/appointments_mailer");
+
+// Variables
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://' + config.appointmentUser.name + ':' + config.appointmentUser.password + '@cluster0.lj881zv.mongodb.net/?retryWrites=true&w=majority';
+//const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/UserDB';
 
 // MQTT Client
+const mailer = new appointments_mailer
 const mqttClient = new mqttHandler(config.appointmentUser.handler)
 mqttClient.connect()
+
+// Connect to MongoDB
+const mongooseClient = mongoose.createConnection(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true}, function (err) {
+    if (err) {
+        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
+        console.error(err.stack);
+        process.exit(1);
+    }
+    console.log(`Connected to MongoDB with URI: ${mongoURI}`);
+})
+
+// Model creation
+const timeSlotModel = mongooseClient.model('timeslot', timeslotSchema)
 
 // MQTT subscriptions
 mqttClient.subscribeTopic('test')
@@ -16,6 +37,9 @@ mqttClient.mqttClient.on('message', function (topic, message) {
     console.log(intermediary)
 
     switch (topic) {
+        case 'test':
+            testAppointment(message)
+            break;
         case 'schema':
             mqttClient.sendMessage('testAppointment', "newClinic")
             break;
@@ -46,6 +70,13 @@ function testAppointment(message) {
         console.log('successfully saved')
     });
     mqttClient.sendMessage(message.id + '/appointmentResponse', JSON.stringify(newClinic))
+}
+
+function bookAppointment(input) {
+    // mongodb manipulation
+    mailer.sendAppointmentMail(input.recipient, input.timeslot, input.clinic)
+    mqttClient.sendMessage('bookAppointment', 'ADD MESSAGE HERE') //TODO: Add a message here.
+    //mqtt message response
 }
 
 module.exports = mqttClient;
--- server/appointments/controllers/appointments_mailer.js
+++ server/appointments/controllers/appointments_mailer.js
@@ -8,57 +8,57 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+export class appointments_mailer {
 //Dummy test data
-/*
-const recipient = "burakaskan2001@gmail.com"
-const timeslot = {
-    Dentist: "john",
-    StartingTime: "01",
-    EndingTime: "02"
-}
-const clinic = {
-    addressLocation: "rando",
-    addressEmail: "somegmail",
-    phoneNumber: "072312"
-}*/
-
-transporter.sendMail(option, async function(err, info) {
-    if (err) {
-        console.log('Email could not be sent.')
-        console.log(err)
-        return;
+    /*
+    const recipient = "burakaskan2001@gmail.com"
+    const timeslot = {
+        Dentist: "john",
+        StartingTime: "01",
+        EndingTime: "02"
     }
-    console.log("Sent: " + info.response)
-})
-
-function sendAppointmentMail (recipient, timeslot, clinic) {
-    const option = {
-        from: "DentistryAutomated@hotmail.com",
-        to: recipient,
-        subject: "Your Dentist Appointment",
-        text: "You have booked a time with "
-            + timeslot.Dentist +
-            ". The time slot is from "
-            + timeslot.StartingTime
-            + " to " + timeslot.EndingTime
-            + ". The clinic is located in "
-            + clinic.addressLocation + ". "
-            + "For contacting the clinic either email: " + clinic.addressEmail
-            + " or call: " + clinic.phoneNumber
-            + ". Hope you have enjoyed our service and use our website again for dental bookings!",
-        html: '<nobr>You have booked a time with</nobr>' + timeslot.Dentist +
-            '<nobr>. The time slot is from "</nobr>' + timeslot.StartingTime +
-            '<nobr> to </nobr>' + timeslot.EndingTime +
-            '<nobr>. The clinic is located in </nobr>' + clinic.addressLocation +
-            '<nobr>. For contacting the clinic either email: </nobr>' + clinic.addressEmail +
-            '<nobr> or call: </nobr>' + clinic.phoneNumber +
-            '<nobr>. Hope you have enjoyed our service and use our website again for dental bookings!<br/></nobr> <img src = "cid:toothLogo"/>',
-        attachments: [
-            {
-                filename: 'toothLogo.png',
-                path:'./toothLogo.png',
-                cid: 'toothLogo'
+    const clinic = {
+        addressLocation: "rando",
+        addressEmail: "somegmail",
+        phoneNumber: "072312"
+    }*/
+    sendAppointmentMail (recipient, timeslot, clinic) {
+        const option = {
+            from: "DentistryAutomated@hotmail.com",
+            to: recipient,
+            subject: "Your Dentist Appointment",
+            text: "You have booked a time with "
+                + timeslot.Dentist +
+                ". The time slot is from "
+                + timeslot.StartingTime
+                + " to " + timeslot.EndingTime
+                + ". The clinic is located in "
+                + clinic.addressLocation + ". "
+                + "For contacting the clinic either email: " + clinic.addressEmail
+                + " or call: " + clinic.phoneNumber
+                + ". Hope you have enjoyed our service and use our website again for dental bookings!",
+            html: '<nobr>You have booked a time with</nobr>' + timeslot.Dentist +
+                '<nobr>. The time slot is from "</nobr>' + timeslot.StartingTime +
+                '<nobr> to </nobr>' + timeslot.EndingTime +
+                '<nobr>. The clinic is located in </nobr>' + clinic.addressLocation +
+                '<nobr>. For contacting the clinic either email: </nobr>' + clinic.addressEmail +
+                '<nobr> or call: </nobr>' + clinic.phoneNumber +
+                '<nobr>. Hope you have enjoyed our service and use our website again for dental bookings!<br/></nobr> <img src = "cid:toothLogo"/>',
+            attachments: [
+                {
+                    filename: 'toothLogo.png',
+                    path:'./toothLogo.png',
+                    cid: 'toothLogo'
+                }
+            ]
+        }
+        transporter.sendMail(option, async function(err, info) {
+            if (err) {
+                console.log('Email could not be sent.')
+                console.log(err)
+                return;
             }
-        ]
+            console.log("Sent: " + info.response)
+        })
     }
 }
