a155ea3b44bfcf3f58fbdee0b17c1a7f5a6e073d
aieh <aieh@student.chalmers.net>
2022-12-02 02:02:34 +0400

    initial backend implementation

--- server/clinic_data/clinic_data_app.js
+++ server/clinic_data/clinic_data_app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const mqttHandler = require('../helpers/mqtt_handler');
 const config = require('../helpers/config');
+const clinicSchema = require('../helpers/schemas/clinic');
 
 // Variables
 const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://' + config.clinicUser.name + ':' + config.clinicUser.password + '@cluster0.lj881zv.mongodb.net/?retryWrites=true&w=majority';
@@ -26,6 +27,23 @@ const mongooseClient = mongoose.createConnection(mongoURI, {useNewUrlParser: tru
 // MQTT subscriptions
 mqttClient.subscribeTopic('test')
 
+const editProfile = async (req, res) => {
+    const clinicModel = mongooseClient.model('clinic', clinicSchema);
+    const {email} = req.body
+    const clinic = await clinicModel.find({email})
+    if (clinic) {
+        clinic.name = req.body.name || clinic.name;
+        clinic.owner = req.body.owner || clinic.owner;
+        clinic.address = req.body.address || clinic.address;
+        clinic.email = req.body.email || clinic.email;
+        clinic.password = req.body.password || clinic.password;
+        res.status(200).json({clinic});
+    } else {
+        res.status(404).json("Clinic not found");
+    }
+
+}
+
 // When a message arrives, respond to it or propagate it further
 mqttClient.mqttClient.on('message', function (topic, message) {
     console.log(config.clinicUser.handler + " service received MQTT message")
@@ -35,6 +53,8 @@ mqttClient.mqttClient.on('message', function (topic, message) {
         case 'test':
             mqttClient.sendMessage('testAppointment', 'Testing callback')
             break;
+        case 'editInfo':
+            mqttClient.sendMessage('editInfo', editProfile(message))
     }
 });
 
