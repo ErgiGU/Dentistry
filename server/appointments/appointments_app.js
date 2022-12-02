cf1db68c99cd9546fe4d5c3abde9940fce286cca
askan <askan@student.chalmers.se>
2022-12-02 21:30:11 +0100

    Rebase conflict fix.

--- server/appointments/appointments_app.js
+++ server/appointments/appointments_app.js
@@ -1,29 +1,13 @@
-const mongoose = require('mongoose');
 const mqttHandler = require('../helpers/mqtt_handler');
 const config = require('../helpers/config');
 const {appointments_mailer} = require("./controllers/appointments_mailer");
 
 // Variables
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://' + config.appointmentUser.name + ':' + config.appointmentUser.password + '@cluster0.lj881zv.mongodb.net/?retryWrites=true&w=majority';
-//const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/UserDB';
 
 // MQTT Client
 const mailer = new appointments_mailer
 const mqttClient = new mqttHandler(config.appointmentUser.handler)
 mqttClient.connect()
-
-// Connect to MongoDB
-const mongooseClient = mongoose.createConnection(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true}, function (err) {
-    if (err) {
-        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
-        console.error(err.stack);
-        process.exit(1);
-    }
-    console.log(`Connected to MongoDB with URI: ${mongoURI}`);
-})
-
-// Model creation
-const timeSlotModel = mongooseClient.model('timeslot', timeslotSchema)
 
 // MQTT subscriptions
 mqttClient.subscribeTopic('test')
@@ -36,9 +20,6 @@ mqttClient.mqttClient.on('message', function (topic, message) {
     console.log(intermediary)
 
     switch (topic) {
-        case 'test':
-            testAppointment(message)
-            break;
         case 'schema':
             mqttClient.sendMessage('testAppointment', "newClinic")
             break;
