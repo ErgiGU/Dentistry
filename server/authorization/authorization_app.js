6b21e870af89289a5756b8cf79d28a840d877c43
ergi <ergi@student.chalmers.se>
2022-12-02 03:50:02 +0100

    Frontend-backend connection for registration
    #1 Established a connection from the frontend to the backend by setting up an mqtt client in the frontend that successfully communicates with the backend. The backend needs to send the data to the DB via mongoose, which will be done tomorrow.

--- server/authorization/authorization_app.js
+++ server/authorization/authorization_app.js
@@ -1,5 +1,6 @@
 const mqttHandler = require('../helpers/mqtt_handler');
 const config = require('../helpers/config');
+const registerClinic = require('./controllers/authorization_controller');
 
 // MQTT Client
 const mqttClient = new mqttHandler(config.authorizationUser.handler)
@@ -7,6 +8,7 @@ mqttClient.connect()
 
 // MQTT subscriptions
 mqttClient.subscribeTopic('auth')
+mqttClient.subscribeTopic('registration');
 
 // When a message arrives, respond to it or propagate it further
 mqttClient.mqttClient.on('message', function (topic, message) {
@@ -15,13 +17,18 @@ mqttClient.mqttClient.on('message', function (topic, message) {
     console.log(intermediary);
 
     switch (topic) {
-        case "test":
-            mqttClient.sendMessage('testAppointment', 'Testing callback')
-            break;
         case 'auth':
             mqttClient.sendMessage('authTest', 'Authorization confirmed')
             break;
+        case 'registration':
+           /* registerClinic.registerClinic(JSON.parse(message)).then(
+
+            );*/
+            console.log("got the message");
+            mqttClient.sendMessage('registrationResponse', 'Heres the response');
+            break;
     }
+
 });
 
 // Function declaration
