1f94a135b09e1665973408f6339b39cb9d95d2d5
sejal <guskanase@student.gu.se>
2022-12-02 21:16:51 +0100

    Resolved conflicts.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import Registration from "./ClinicRegistration/Registration";
 import React from 'react';
+import {AboutUs} from "./AboutUs/AboutUs";
 import Home from "./home/Home";
 
 
@@ -14,6 +15,8 @@ function App() {
         //you can add more routes here, just follow the same format
         <Routes>
             <Route path = "/" element = {< Registration />} />
+            <Route path = "/aboutUs" element = {< AboutUs />} />
+            <Route />
             <Route path={"/Home"} element= {<Home/>} />
         </Routes>
     );
