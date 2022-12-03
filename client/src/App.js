e03b9b7bed23c73e55b4ce3e13a6c5a55eba45e9
aieh <aieh@student.chalmers.net>
2022-12-04 00:48:49 +0400

    main rebase

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import {
     Routes, //replaces "Switch" used till v5
     Route,
 } from "react-router-dom";
-import Registration from "./ClinicRegistration/Registration";
+import {Registration} from "./ClinicRegistration/Registration";
+import {MyInformation} from "./MyInformation/MyInformation"
 import React from 'react';
-import Home from "./home/Home";
 
 
 function App() {
@@ -13,8 +13,10 @@ function App() {
     return(
         //you can add more routes here, just follow the same format
         <Routes>
-            <Route path = "/" element = {< Registration />} />
-            <Route path={"/Home"} element= {<Home/>} />
+            <Route path = "/n" element = {< Registration />} />
+            <Route />
+            <Route path = "/" element = {< MyInformation />} />
+            <Route />
         </Routes>
     );
 
