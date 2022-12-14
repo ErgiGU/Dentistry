418eaead07b68ed59e9fb9e22437c6a4ded8569f
gusciuag <102141705+gusciuag@users.noreply.github.com>
2022-12-14 13:28:30 +0100

    Rebase

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,15 @@ import {MyInformation} from "./MyInformation/MyInformation"
 import React from 'react';
 import Login from "./ClinicLogin/Login";
 import Home from "./home/Home";
-
-
+import ViewAppointments from "./ViewAppointments/viewAppointments"
+import Navbar from './common_components/navbar'
 function App() {
 
     return(
         //you can add more routes here, just follow the same format
+        <>
+            <Navbar/>
+
         <Routes>
             <Route path ="/" />
             <Route path ="/registration" element= {< Registration />} />
@@ -24,6 +27,7 @@ function App() {
             <Route path ="/map" element = {< MapPage />} />
             <Route path ="/profile" element = {< MyInformation />} />
         </Routes>
+        </>
     );
 }
 
