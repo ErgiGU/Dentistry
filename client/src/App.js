ca5c1c33f0e81a8e3e75910315eb304924c9aefa
gusciuag <102141705+gusciuag@users.noreply.github.com>
2022-12-14 13:28:30 +0100

    View appointments responsive design

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,15 @@ import MapPage from "./clinics/MapPage"
 import React from 'react';
 import Login from "./ClinicLogin/Login";
 import Home from "./home/Home";
-
-
+import ViewAppointments from "./ViewAppointments/viewAppointments"
+import Navbar from './common_components/navbar'
 function App() {
 
     return(
         //you can add more routes here, just follow the same format
+        <>
+            <Navbar/>
+
         <Routes>
             <Route path ="/" />
             <Route path ="/registration" element= {< Registration />} />
@@ -22,6 +25,7 @@ function App() {
             <Route path ="/Home" element= {< Home />} />
             <Route path ="/map" element = {< MapPage />} />
         </Routes>
+        </>
     );
 }
 
