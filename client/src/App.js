b3d83d233c566d15cce62a9c186f16b9ee9e6b8b
gusciuag <102141705+gusciuag@users.noreply.github.com>
2022-12-16 15:11:06 +0100

    Frontend methods to receive the appointment infromation from the backend Helps #8

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,30 @@ import {
     Routes, //replaces "Switch" used till v5
     Route,
 } from "react-router-dom";
+
 import Registration from "./ClinicRegistration/Registration";
+import MapPage from "./clinics/MapPage"
 import React from 'react';
+import Login from "./ClinicLogin/Login";
 import Home from "./home/Home";
-
-
+import ViewAppointments from "./ViewAppointments/viewAppointments"
+import Navbar from './common_components/navbar'
 function App() {
 
     return(
         //you can add more routes here, just follow the same format
-        <Routes>
-            <Route path = "/" element = {< Registration />} />
-            <Route path={"/Home"} element= {<Home/>} />
-        </Routes>
-    );
+        <>
+            <Navbar/>
 
+        <Routes>
+            <Route path ="/" />
+            <Route path ="/registration" element= {< Registration />} />
+            <Route path ="/login" element= {< Login />} />
+            <Route path ="/Home" element= {< Home />} />
+            <Route path ="/map" element = {< MapPage />} />
+        </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ client/src/ViewAppointments/components/timeslotCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function BookedTimeslots(props) {
+
+    const patientName = props.patientName
+    const dentistName = props.dentistName
+    const timeslotTime = props.timeslotStarttime
+    const text = props.patient.text
+    return (
+    <div key={patientName + dentistName + timeslotTime} className="card1">
+        <div className="card-body">
+            <div className="row align-items-end">
+                <div className="col">TIME
+                    <div className="text">{timeslotTime}</div>
+                </div>
+                <div className="col">PATIENT
+                    <div className="text">{patientName}</div>
+                </div>
+                <div className="col">ADDITIONAL INFORMATION
+                    <div className="text">{text}</div>
+                </div>
+                <div className="col">DOCTOR
+                    <div className="text">{dentistName}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    )
+}
