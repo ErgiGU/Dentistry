41f84f94519dd4a323926c1bc8810b77c7c255d8
sejal <guskanase@student.gu.se>
2023-01-02 15:28:02 +0100

    resolved conflicts.

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,9 @@ import {
     Routes, //replaces "Switch" used till v5
     Route,
 } from "react-router-dom";
-
-import Registration from "./ClinicRegistration/Registration";
-import MapPage from "./clinics/MapPage"
-import {MyInformation} from "./MyInformation/MyInformation"
+import {Registration} from "./ClinicRegistration/Registration";
 import React from 'react';
-import Login from "./ClinicLogin/Login";
 import {AboutUs} from "./AboutUs/AboutUs";
-import Home from "./home/Home";
 
 
 function App() {
@@ -18,16 +13,9 @@ function App() {
     return(
         //you can add more routes here, just follow the same format
         <Routes>
-            <Route path ="/" />
-            <Route path ="/registration" element= {< Registration />} />
-            <Route path ="/login" element= {< Login />} />
-            <Route path ="/Home" element= {< Home />} />
-            <Route path ="/map" element = {< MapPage />} />
-            <Route path ="/profile" element = {< MyInformation />} />
             <Route path = "/" element = {< Registration />} />
             <Route path = "/aboutUs" element = {< AboutUs />} />
             <Route />
-            <Route path={"/Home"} element= {<Home/>} />
         </Routes>
     );
 
