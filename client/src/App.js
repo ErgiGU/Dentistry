4ca3d48cd1f70a47cb67b0d80cff2329798dad9f
sejal <guskanase@student.gu.se>
2023-01-02 13:55:37 +0100

    rebase

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,13 @@ import {
     Routes, //replaces "Switch" used till v5
     Route,
 } from "react-router-dom";
-import {Registration} from "./ClinicRegistration/Registration";
+import Registration from "./ClinicRegistration/Registration";
 import React from 'react';
-import {AboutUs} from "./AboutUs/AboutUs";
+import {AboutUs} from "./AboutUsPage/AboutUs";
+import {YourDentist} from "./AboutUsPage/YourDentist";
+import {LisebergDentist} from "./AboutUsPage/LisebergDentist";
+import {ToothFairy} from "./AboutUsPage/ToothFairy";
+import {TheCrown} from "./AboutUsPage/TheCrown";
 
 
 function App() {
@@ -15,6 +19,10 @@ function App() {
         <Routes>
             <Route path = "/" element = {< Registration />} />
             <Route path = "/aboutUs" element = {< AboutUs />} />
+            <Route path = "/aboutUs/yourDentist" element = {< YourDentist />} />
+            <Route path = "/aboutUs/lisebergDentist" element = {< LisebergDentist />} />
+            <Route path = "/aboutUs/toothFairy" element = {< ToothFairy />} />
+            <Route path = "/aboutUs/theCrown" element = {< TheCrown />} />
             <Route />
         </Routes>
     );
